36e24d1ef7ca40f6755ce512a4027c0d4867d609
atanas-sk <109441195+atanas-sk@users.noreply.github.com>
2022-08-16 15:31:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById("rate");
+var output = document.getElementById("value");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
 }
-        
+
+
+function compute() {
+    const resultsdiv = document.getElementById('computation-text');
+    const deposit = document.getElementById("principal");
+    const rate1 = document.getElementById("rate");
+    const years1 = document.getElementById("years");
+
+
+    document.getElementById('computation-text').innerHTML = '';
+
+  if (deposit.value == 0 || rate1.value == 0 || years1.value == 0) {
+    const p = document.createElement('p');
+    const pText = document.createTextNode('Please enter all fields');
+
+    resultsdiv.style.color = 'red';
+    resultsdiv.style.fontStyle = 'italic';
+    p.appendChild(pText);
+    resultsdiv.appendChild(p);
+
+
+  } else {
+    const result = document.getElementById('result');
+    
+    const currentdate = new Date().getFullYear();
+    const p = document.createElement('p');
+    const t = document.createElement('p');
+    const v = document.createElement('p');
+    const x = document.createElement('p');
+    const time = currentdate + parseInt(years1.value);
+    const amount = (deposit.value * rate1.value)/100 * parseInt(years1.value);
+    
+
+    const pText = document.createTextNode('If you deposit '+ principal.value);
+    const tText = document.createTextNode('at an interest rate of  '+ rate1.value+'%');
+    const vText = document.createTextNode('You will receive an amount of  ' + amount);
+    const xText = document.createTextNode('In the year  ' + time);
+
+    
+    resultsdiv.style.color = 'black';
+    resultsdiv.style.fontStyle = 'normal';
+    p.appendChild(pText);
+    resultsdiv.appendChild(p);
+    t.appendChild(tText);
+    resultsdiv.appendChild(t);
+    v.appendChild(vText);
+    resultsdiv.appendChild(v);
+    x.appendChild(xText);
+    resultsdiv.appendChild(x);
+    
+ 
+    
+    principal.value.style.background = 'black';
+  }
+}
